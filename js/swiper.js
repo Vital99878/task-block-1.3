14499d16aa09acac6ba599c0f583957b36e77f95
Vital <Lihoy84@yandex.ru>
2020-12-04 13:37:04 +0300

    linked swiper

---
+++ js/swiper.js
@@ -0,0 +1,18 @@
+
+
+new Swiper('.brands', {
+
+//  navigation: {
+//    nextEl: '.swiper-button-next',
+//    prevEl: '.swiper-button-prev'
+//  },
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  }, scrollbar: {
+    el: '.swiper-scrollbar',
+    draggable: true
+  }
+
+});
