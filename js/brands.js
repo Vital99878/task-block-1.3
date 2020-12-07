fc84d2fdd8d14b37eb72918275a11113e469196f
Vital <Lihoy84@yandex.ru>
2020-12-07 18:32:49 +0300

    ref: flexblie item

--- js/brands.js
+++ js/brands.js
@@ -1,5 +1,4 @@
 let more_btn = document.querySelector('.more');
-let hidden_card = document.querySelector('.brands__item:nth-child(8)');
 let cards = document.querySelector('.brands__list');
 
 
@@ -9,7 +8,7 @@ more_btn.addEventListener('click', function ( ) {
     more_btn.textContent = "Скрыть";
     more_btn.style.backgroundImage = "url('../img/hide-arrow.svg')";
     cards.style.maxHeight = "1000";
-    hidden_card.classList.add('brands__card--hide')
+
   }else {
     more_btn.textContent = "Показать все";
     more_btn.style.backgroundImage = "url('../img/more.svg')";
@@ -18,22 +17,19 @@ more_btn.addEventListener('click', function ( ) {
 })
 
 const mySwiper = new Swiper('.swiper-container', {
-  // Optional parameters
-//  loop: true,
 
-  // If we need pagination
   pagination: {
     el: '.swiper-pagination',
     click: true
   },
 
-  // And if we need scrollbar
   scrollbar: {
     el: '.swiper-scrollbar',
   },
+//  spaceBetween: 30
 })
 
 if(window.innerWidth >= 576) {
-  mySwiper.destroy(false, false);
+  mySwiper.destroy(true, true);
   console.log("destroy")
 }
