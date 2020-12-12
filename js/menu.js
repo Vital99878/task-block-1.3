e63b42d2e01c7f472b2125ef5c7fd27fc0ba7167
Vital <Lihoy84@yandex.ru>
2020-12-12 14:49:27 +0300

    modal new comp

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,5 @@
 let menu__icon = document.querySelector('.header__icon-menu');
-let menu__close = document.querySelector('.header__icon--close-menu');
+let menu__close = document.querySelector('.modal-menu__icon--close');
 let menu__modal = document.querySelector('.modal-menu');
 let body = document.querySelector('body');
 
